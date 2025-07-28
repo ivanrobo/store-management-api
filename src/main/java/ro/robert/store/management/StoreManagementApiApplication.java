67b8f06b8db935501a98ac8825be4920c3a09892
ivanrobo <ivanrobert_07@yahoo.com>
2025-07-28 22:13:22 +0300

    chore(setup): create Spring Boot Project

---
+++ src/main/java/ro/robert/store/management/StoreManagementApiApplication.java
@@ -0,0 +1,13 @@
+package ro.robert.store.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementApiApplication.class, args);
+	}
+
+}
