1542dba86fffb8fff418f95088b56dcb5ff511ad
ivanrobo <ivanrobert_07@yahoo.com>
2025-07-29 20:30:48 +0300

    feat(product): add unsupported update request

--- src/main/java/ro/robert/store/management/product/entity/request/ProductUpdateRequest.java
+++ src/main/java/ro/robert/store/management/product/entity/request/ProductUpdateRequest.java
@@ -10,7 +10,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(
     use = JsonTypeInfo.Id.NAME,
     include = JsonTypeInfo.As.PROPERTY,
-    property = "type"
+    property = "type",
+    defaultImpl = UnsupportedUpdateRequest.class
 )
 @JsonSubTypes({
     @JsonSubTypes.Type(value = UpdatePriceRequest.class, name = "UpdatePriceRequest"),
