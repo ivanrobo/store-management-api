c328c46ad8d24e9d49f04857ba4bfc5de769dcac
ivanrobo <ivanrobert_07@yahoo.com>
2025-07-29 19:09:29 +0300

    feat(product): add get all products paginated endpoint

--- src/main/java/ro/robert/store/management/product/boundary/ProductController.java
+++ src/main/java/ro/robert/store/management/product/boundary/ProductController.java
@@ -2,6 +2,10 @@ package ro.robert.store.management.product.boundary;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,5 +24,22 @@ public class ProductController {
     public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductCreateRequest request) {
         ProductResponse response = productService.createProduct(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+    
+    @GetMapping
+    public ResponseEntity<Page<ProductResponse>> getAllProducts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String sortBy,
+            @RequestParam(defaultValue = "ASC") Sort.Direction sortDirection) {
+        
+        Sort sort = Sort.unsorted();
+        if (sortBy != null && !sortBy.isEmpty()) {
+            sort = Sort.by(sortDirection, sortBy);
+        }
+        
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<ProductResponse> products = productService.getAllProducts(pageable);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 }
