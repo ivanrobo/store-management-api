bcd7ee90c42c6ae302915869d39aa8dfef3a9117
ivanrobo <ivanrobert_07@yahoo.com>
2025-07-30 21:42:09 +0300

    feat(security): activate spring security

--- src/main/java/ro/robert/store/management/StoreManagementApiApplication.java
+++ src/main/java/ro/robert/store/management/StoreManagementApiApplication.java
@@ -2,9 +2,8 @@ package ro.robert.store.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class StoreManagementApiApplication {
 
 	public static void main(String[] args) {
