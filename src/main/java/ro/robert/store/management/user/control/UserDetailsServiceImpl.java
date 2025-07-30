6e70e28a840adb0752b4c3cd9b2d742dfd27a9e9
ivanrobo <ivanrobert_07@yahoo.com>
2025-07-30 21:22:36 +0300

    feat(security): add UserDetailsService implementation

---
+++ src/main/java/ro/robert/store/management/user/control/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package ro.robert.store.management.user.control;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ro.robert.store.management.user.boundary.UserRepository;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> {
+                    log.warn("User not found: {}", username);
+                    return new UsernameNotFoundException("User not found: " + username);
+                });
+    }
+}
